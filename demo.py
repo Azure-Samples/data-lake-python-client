ea50e47324f889ef762094ee2bbb2dbb02ed05fb
Saveen Reddy <saveenr@live.com>
2017-01-09 11:11:38 -0800

    Initial commit

---
+++ demo.py
@@ -0,0 +1,21 @@
+# Common Azure imports
+import adal
+import azure.mgmt.resource.resources
+import msrestazure.azure_active_directory
+
+# ADLS imports
+import azure.mgmt.datalake.store
+import azure.datalake.store
+
+# ADLA imports
+import azure.mgmt.datalake.analytics
+
+# All other imports
+import os
+import sys
+import itertools
+
+import adlhelper
+
+print("DEMO STARTING")
+print("DEMO FINISHED")
